787c5a4e818d0982acef71135875e5cef0f57431
Patricia <patricia.jerez-prado@capgemini.com>
2022-03-07 21:55:05 +0100

    Quitada la Seguridad del proyecto.

--- Spring_Tarea_Grupal/src/main/resources/Import.sql
+++ Spring_Tarea_Grupal/src/main/resources/Import.sql
@@ -11,15 +11,3 @@ INSERT INTO productos (descripcion, existencias, nombre, precio_unitario) VALUES
 INSERT INTO ventas (cantidad, iva, subtotal, total, num_cliente, clave) VALUES ('2', '21', '200', '300', '1', '3');
 INSERT INTO ventas (cantidad, iva, subtotal, total, num_cliente, clave) VALUES ('3', '10', '300', '150', '2', '2');
 INSERT INTO ventas (cantidad, iva, subtotal, total, num_cliente, clave) VALUES ('4', '4', '500', '300', '3', '1');
-
-
-INSERT INTO usuarios (username,password,enabled) VALUES('german','$2a$10$5zPAUe1.CWsYH1udDwO/YODf.HIeg5sJsEEKMZj2U85Ryqdlc1LPy',1);
-INSERT INTO usuarios (username,password,enabled) VALUES('patricia','$2a$10$Afb7kuzKRV3vf/iJlvtNn.WbxKWKda6XhHADccPTUgfPXYWot.krW',1);
-INSERT INTO usuarios (username,password,enabled) VALUES('iñigo','$2a$10$NfnJbZcP3qhZne5DKlxaMO/B.otmVNl3ITAuXrbVanve93lp0tTbO',1);
-
-INSERT INTO roles (nombre) VALUES ('ROLE_USER');
-INSERT INTO roles (nombre) VALUES ('ROLE_ADMIN');
-
-INSERT INTO usuarios_roles (usuario_id,role_id) VALUES(1,1);
-INSERT INTO usuarios_roles (usuario_id,role_id) VALUES(2,2);
-INSERT INTO usuarios_roles (usuario_id,role_id) VALUES(3,1);

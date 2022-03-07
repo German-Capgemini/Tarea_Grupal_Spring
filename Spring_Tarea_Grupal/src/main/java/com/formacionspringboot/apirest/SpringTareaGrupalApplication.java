950ace11dc7e170b399c0e96b5618cd68c1793c4
inalvare <inigo.alvarez-hernandez@capgemini.com>
2022-03-07 10:56:50 +0100

    intentando

---
+++ Spring_Tarea_Grupal/src/main/java/com/formacionspringboot/apirest/SpringTareaGrupalApplication.java
@@ -0,0 +1,13 @@
+package com.formacionspringboot.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTareaGrupalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTareaGrupalApplication.class, args);
+	}
+
+}

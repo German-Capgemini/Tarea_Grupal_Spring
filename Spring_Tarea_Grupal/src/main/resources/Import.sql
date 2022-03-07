244ad32ca5839c0ecd6d580c45fcdefbe3c00f6e
German-Capgemini <german-fernando.peralta-romero@capgemini.com>
2022-03-07 11:26:07 +0100

    Import sql

---
+++ Spring_Tarea_Grupal/src/main/resources/Import.sql
@@ -0,0 +1,9 @@
+INSERT INTO clientes (apellidos, nombre, sexo, telefono) VALUES ('Menendez', 'Pablo', 'Varon', '987778866');
+INSERT INTO clientes (apellidos, nombre, sexo, telefono) VALUES ('Suarez', 'Pedro', 'Varon', '985345324');
+INSERT INTO clientes (apellidos, nombre, sexo, telefono) VALUES ('Rodriguez', 'Esteban', 'Varon', '986787656');
+INSERT INTO clientes (apellidos, nombre, sexo, telefono) VALUES ('Martinez', 'Nuria', 'MUjer', '98644321');
+
+INSERT INTO productos (descripcion, existencias, nombre, precio_unitario) VALUES ('Es una lavadora', '300', 'Lavadora 300HT', '200');
+INSERT INTO productos (descripcion, existencias, nombre, precio_unitario) VALUES ('Es un movil', '200', 'Xiaomi Redmi 9', '250');
+INSERT INTO productos (descripcion, existencias, nombre, precio_unitario) VALUES ('Es una batidora', '100', 'Batidora 3000 Max', '50');
+INSERT INTO productos (descripcion, existencias, nombre, precio_unitario) VALUES ('Es una ordenador', '50', 'Ordenador HP 300', '800');

543804292711b99df9f37c2405caaea85b4132f8
inalvare <inigo.alvarez-hernandez@capgemini.com>
2022-03-07 20:24:03 +0100

    corrijo una falta de ortografia

--- Spring_Tarea_Grupal/src/main/resources/Import.sql
+++ Spring_Tarea_Grupal/src/main/resources/Import.sql
@@ -1,7 +1,7 @@
 INSERT INTO clientes (apellidos, nombre, sexo, telefono) VALUES ('Menendez', 'Pablo', 'Varon', '987778866');
 INSERT INTO clientes (apellidos, nombre, sexo, telefono) VALUES ('Suarez', 'Pedro', 'Varon', '985345324');
 INSERT INTO clientes (apellidos, nombre, sexo, telefono) VALUES ('Rodriguez', 'Esteban', 'Varon', '986787656');
-INSERT INTO clientes (apellidos, nombre, sexo, telefono) VALUES ('Martinez', 'Nuria', 'MUjer', '98644321');
+INSERT INTO clientes (apellidos, nombre, sexo, telefono) VALUES ('Martinez', 'Nuria', 'Mujer', '98644321');
 
 INSERT INTO productos (descripcion, existencias, nombre, precio_unitario) VALUES ('Es una lavadora', '300', 'Lavadora 300HT', '200');
 INSERT INTO productos (descripcion, existencias, nombre, precio_unitario) VALUES ('Es un movil', '200', 'Xiaomi Redmi 9', '250');
